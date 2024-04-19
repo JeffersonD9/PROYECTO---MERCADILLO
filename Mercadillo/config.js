4ee01bc98929d26a5ae41c18f359ed8e16c06227
JeffersonD <jeffersonm0922@gmail.com>
2024-04-19 18:42:25 -0500

    Resolviendo Merge

--- Mercadillo/config.js
+++
@@ -1,9 +0,0 @@
-import {config} from 'dotenv'
-
-config()
-
-//Settings 
-
-export const PORT = process.env.PORT || 3000
-export const SECRET_TOKEN = "Secret-Token"
-
--- Mercadillo/routes/TaskRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import{authRequired} from "../MiddleWares/ValidateToken.js"
-
-const router = Router()
-
-router.get('/tasks',authRequired,(req,res)=>{res.send("Task")})
-
-export default router
