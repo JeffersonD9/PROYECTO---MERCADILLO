5d44b0c1b0c18bef92508f06b1af24b7608e8754
ivillamizar5 <ivan.151867@gmail.com>
2024-04-24 00:14:39 -0500

    Se añadio el codigo

--- Mercadillo/Services/ServicesUser.js
+++ Mercadillo/Services/ServicesUser.js
@@ -15,3 +15,38 @@ export async function EncryptPassword(password){
     const passwordHash = await bcrypt.hash(password,10)
     return passwordHash
 }
+
+export async function SearchUser(email){
+
+    try {       
+        const adminFound = await prisma.admin.findUnique({
+            where:{
+                Email:email
+            }
+        })
+    
+        const salesFound = await prisma.usuario.findUnique({
+            where:{
+                Email:email
+            }
+        })
+
+        if (adminFound) {
+            console.log("es Admin")
+            return adminFound.id_Rol
+
+        } else if (salesFound) {
+            console.log("es Vendedor")
+            return salesFound.id_Rol
+
+        } else {
+
+            return null;
+        }
+    } catch (error) {
+        
+        console.error("Error al buscar usuario:", error);
+        throw error;
+    }
+
+}
