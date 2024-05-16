65c1129ff164198179ed8a1ff4480946f3a92337
JeffersonD <jeffersonm0922@gmail.com>
2024-05-16 17:51:10 -0500

    Json test

---
+++ Mercadillo/test/Index.spec.js
@@ -0,0 +1,42 @@
+import app from "../app";
+import request from 'supertest'
+ 
+describe('POST /Login', () => { 
+
+    test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).post('/').send()
+       console.log(responder)
+    })
+})
+
+describe('POST /Register', () => { 
+
+    test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).post('/').send()
+       console.log(responder)
+    })
+})
+
+describe('Get /Admin', () => { 
+
+    test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).get('/').send()
+       console.log(responder)
+    })
+})
+
+describe('Get /Usuario', () => { 
+
+    test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).get('/').send()
+       console.log(responder)
+    })
+})
+
+describe('Get /Admin/Categorias', () => { 
+
+    test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).get('/').send()
+       console.log(responder)
+    })
+})
