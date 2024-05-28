958b8e1ef8deaab4b3e70c8833e894cbcec39491
JeffersonD <jeffersonm0922@gmail.com>
2024-05-27 21:50:55 -0500

    Modified for testing

--- Mercadillo/test/Index.spec.js
+++ Mercadillo/test/Index.spec.js
@@ -4,24 +4,24 @@ import request from 'supertest'
 describe('POST /Login', () => { 
 
     test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).post('/').send()
-       console.log(responder)
+       const responder =  await request(app).post('/MercadilloBucaramanga/Login').send()
+     // console.log(responder)
     })
 })
 
 describe('POST /Register', () => { 
 
     test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).post('/').send()
-       console.log(responder)
+       const responder =  await request(app).get('/MercadilloBucaramanga/Registrar').send()
+       //console.log(responder)
     })
 })
 
 describe('Get /Admin', () => { 
 
     test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).get('/').send()
-       console.log(responder)
+       const responder =  await request(app).get('/MercadilloBucaramanga/Admin').send()
+     //  console.log(responder)
     })
 })
 
@@ -29,7 +29,7 @@ describe('Get /Usuario', () => {
 
     test('Debería responder con codigo 200',async()=>{
        const responder =  await request(app).get('/').send()
-       console.log(responder)
+     //  console.log(responder)
     })
 })
 
@@ -37,6 +37,6 @@ describe('Get /Admin/Categorias', () => {
 
     test('Debería responder con codigo 200',async()=>{
        const responder =  await request(app).get('/').send()
-       console.log(responder)
+     //  console.log(responder)
     })
 })
