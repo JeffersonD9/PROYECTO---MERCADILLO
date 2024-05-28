053282e09368e365f25f6c343f0ca063046dfdc1
JeffersonD <jeffersonm0922@gmail.com>
2024-05-28 17:09:28 -0500

    update

--- Mercadillo/test/Index.spec.js
+++ Mercadillo/test/Index.spec.js
@@ -1,6 +1,8 @@
 import app from "../app";
 import request from 'supertest'
  
+//TESTING DE ROUTINGS GET 
+
 describe('POST /Login', () => { 
 
     test('Debería responder con codigo 200',async()=>{
@@ -25,10 +27,11 @@ describe('Get /Admin', () => {
     })
 })
 
+
 describe('Get /Usuario', () => { 
 
     test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).get('/').send()
+       const responder =  await request(app).get('/MercadilloBucaramanga/Usuario').send()
      //  console.log(responder)
     })
 })
@@ -36,7 +39,25 @@ describe('Get /Usuario', () => {
 describe('Get /Admin/Categorias', () => { 
 
     test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).get('/').send()
+       const responder =  await request(app).get('/MercadilloBucaramanga/Admin/Categorias').send()
+     //  console.log(responder)
+    })
+})
+
+//TESTING ROUTING POST
+
+describe('Post /Admin', () => { 
+
+    test('Debería responder con codigo 404',async()=>{
+       const responder =  await request(app).post('/MercadilloBucaramanga/Admin').send()
+     //  console.log(responder)
+    })
+})
+
+describe('Post /Admin', () => { 
+
+    test('Debería responder con codigo 404',async()=>{
+       const responder =  await request(app).post('/MercadilloBucaramanga/Usuario').send()
      //  console.log(responder)
     })
 })
