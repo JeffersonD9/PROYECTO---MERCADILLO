9542b169bee1071248c0aab52aa22277992ba66c
JeffersonD <jeffersonm0922@gmail.com>
2024-05-29 23:31:22 -0500

    Update Pruebas de Humo

--- Mercadillo/test/Index.spec.js
+++ Mercadillo/test/Index.spec.js
@@ -3,61 +3,125 @@ import request from 'supertest'
  
 //TESTING DE ROUTINGS GET 
 
-describe('POST /Login', () => { 
-
-    test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).get('/MercadilloBucaramanga/Login').send()
-     // console.log(responder)
-    })
-})
-
-describe('POST /Register', () => { 
-
-    test('Debería responder con codigo 200',async()=>{
-       const responder =  await request(app).get('/MercadilloBucaramanga/Registrar').send()
-       //console.log(responder)
-    })
-})
-
 describe('Get /Admin', () => { 
 
-    test('Debería responder con codigo 200',async()=>{
+    test('Debería responder con codigo 302',async()=>{
        const responder =  await request(app).get('/MercadilloBucaramanga/Admin').send()
-     //  console.log(responder)
+       expect(responder.statusCode).toBe(302)
     })
 })
 
-
-describe('Get /Usuario', () => { 
+describe('Get /MercadilloBucaramanga', () => { 
 
     test('Debería responder con codigo 200',async()=>{
+       const responder =  await request(app).get('/MercadilloBucaramanga').send()
+       expect(responder.statusCode).toBe(200)
+    })
+})
+
+describe('Get /Admin/Usuarios', () => { 
+
+    test('Debería responder con codigo 302',async()=>{
+       const responder =  await request(app).get('/MercadilloBucaramanga/Admin/Usuarios').send()
+       expect(responder.statusCode).toBe(302)
+    })
+})
+
+describe('Get /Usuario/', () => { 
+
+    test('Debería responder con codigo 302',async()=>{
        const responder =  await request(app).get('/MercadilloBucaramanga/Usuario').send()
-     //  console.log(responder)
+       expect(responder.statusCode).toBe(302)
     })
 })
 
 describe('Get /Admin/Categorias', () => { 
 
-    test('Debería responder con codigo 200',async()=>{
+    test('Debería responder con codigo 302',async()=>{
        const responder =  await request(app).get('/MercadilloBucaramanga/Admin/Categorias').send()
-     //  console.log(responder)
+       expect(responder.statusCode).toBe(302)
     })
 })
 
 //TESTING ROUTING POST
 
-describe('Post /Admin', () => { 
+describe('POST /Productos', () => { 
 
-    test('Debería responder con codigo 404',async()=>{
-       const responder =  await request(app).post('/MercadilloBucaramanga/Admin').send()
-     //  console.log(responder)
+    test('Debería responder con codigo 201',async()=>{
+       const responder =  await request(app).get('/MercadilloBucaramanga/Productos').send(
+       )
+       expect(responder.status).toBe(302)
     })
 })
 
-describe('Post /Admin', () => { 
+describe('POST /Admin/Catalogos', () => { 
+
+    test('Debería responder con codigo 201',async()=>{
+       const responder =  await request(app).post('/MercadilloBucaramanga/Admin/Catalogos').send(
+        {
+            Nombre: "Manzanas"
+        }
+       )
+       console.log(responder.body.Nombre)
+       expect(responder.status).toBe(302)
+    })
+})
+
+describe('POST /Admin/Categorias', () => { 
+
+    test('Debería responder con codigo 201',async()=>{
+       const responder =  await request(app).post('/MercadilloBucaramanga/Admin/Categorias').send(
+        {
+            Nombre: "Limon Mandarin"
+        }
+       )
+       console.log(responder.body.Nombre)
+       expect(responder.status).toBe(302)
+    })
+})
+describe('POST /Login', () => { 
 
     test('Debería responder con codigo 404',async()=>{
-       const responder =  await request(app).post('/MercadilloBucaramanga/Usuario').send()
-     //  console.log(responder)
+       const responder =  await request(app).post('/MercadilloBucaramanga/Login').send(
+        {
+            Email:"Email3",
+            Password :"Admin123",
+        }
+       )
+       console.log(responder.body)
+       expect(responder.status).toBe(404)
+       expect(responder.body.message).toBe('El Usuario No Existe');
+    })
+})
+
+describe('POST /Login', () => { 
+
+    test('Debería responder con codigo 201',async()=>{
+       const responder =  await request(app).post('/MercadilloBucaramanga/Login').send(
+        {
+            Email:"Email",
+            Password :"Admin123",
+        }
+       )
+       console.log(responder.body)
+       expect(responder.status).toBe(201)
+    })
+})
+
+describe('POST /Register', () => { 
+
+    test('Debería responder con codigo 201',async()=>{
+       const responder =  await request(app).get('/MercadilloBucaramanga/Registrar').send(
+        {
+            Nombres: "Ivan",
+            Apellidos: "Caceres",
+            Celular: "324522010",
+            UserName: "Ivan204",
+            Email: "Ivan@vendedor.com",         
+            Password: "1005369500Jm.",
+            Imagen : "hola"
+        }
+       )
+       expect(responder.statusCode).toBe(200)
     })
 })
